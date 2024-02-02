80edbef6dd0bab89e60c1c0cfa4718a7345705d6
SatadipIITG11 <142224347+SatadipIITG11@users.noreply.github.com>
2024-02-03 02:54:33 +0530

    Bhai metamask connect nhi ho rha crying

--- src/loginpage.jsx
+++ src/loginpage.jsx
@@ -1,10 +1,72 @@
 
 import React,{ useState } from 'react';
 import './loginpage.css'
+import {ethers} from 'ethers'
+import Web3 from 'web3';
+
+
 
 function Loginpage() {
 
  const[toggleState,setToggle]=useState(1);
+
+ const connectWalletHandler = async () => {
+  window.web3 = new Web3(window.ethereum)
+  if (window.ethereum && window.ethereum.isMetaMask) {
+    console.log('MetaMask Here!');
+    
+
+   const accounts=await window.ethereum.request({ method: 'eth_requestAccounts'})
+    // .then(results => {
+    //   console.log(results[0]);
+      
+    //   // getAccountBalance(result[0]);
+    // })
+    // .catch(error => {
+    //   console.log(error.message);
+    
+    // });
+    try{
+      console.log(accounts[0]);
+    }catch(e){
+      console.log(e);
+    }
+
+  } else {
+    console.log('Need to install MetaMask');
+    
+  }
+}
+
+// update account, will cause component re-render
+// const accountChangedHandler = (newAccount) => {
+  
+//   getAccountBalance(newAccount.toString());
+// }
+
+// const getAccountBalance = (account) => {
+//   window.ethereum.request({method: 'eth_getBalance', params: [account, 'latest']})
+//   .then(balance => {
+//     console.log(ethers.utils.formatEther(balance));
+//   })
+//   .catch(error => {
+//     console.log(error.message);
+//   });
+// };
+
+// const chainChangedHandler = () => {
+//   // reload the page to avoid any errors with chain change mid use of application
+//   window.location.reload();
+// }
+
+
+// listen for account changes
+// window.ethereum.on('accountsChanged', accountChangedHandler);
+
+// window.ethereum.on('chainChanged', chainChangedHandler);
+
+
+
   return (
 
     <div className="LOGIN-PAGE">
@@ -23,9 +85,11 @@ function Loginpage() {
          Log In
         </div>
 
-        <div className="metamask">  
+        <button type="button" className="metamask" id='connectWallet' value="Connect Wallet" onClick={connectWalletHandler}>  
          Continue with Metamask
-       </div>
+       </button>
+
+       
 
     </div>
 
