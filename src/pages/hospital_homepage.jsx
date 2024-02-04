95b7a96a63cb0a43dd90789bab1d6ef92a3915c7
SatadipIITG11 <142224347+SatadipIITG11@users.noreply.github.com>
2024-02-04 15:19:58 +0530

    Made the static layout of hospital home page but javascript add karni baki hai

--- src/pages/hospital_homepage.jsx
+++ src/pages/hospital_homepage.jsx
@@ -12,6 +12,26 @@ function HospitalHomepage() {
           </div>
           <div id="upload">Upload</div>
        </div>
+    {/*Here i have to apply a js script which can render with respect to the existence of user
+     Kam baccha hai eske bad user existence check karna padega */}
+      <div className="bodyhospi">
+        <div className="details">
+          <div className="name">
+             <div className="nametext">Name:</div>
+             <div className="namebox"></div>
+          </div>
+          <div id="id">
+            <div className="nametext">Id:</div>
+            <div className="namebox"></div>
+          </div>
+        </div>
+           <div className="blockshospi">
+            <div className="box1 box"></div>
+            <div className="box2 box"></div>
+            <div className="box3 box"></div>
+            <div className="box4 box"></div>
+           </div>
+      </div>
     </div>
   )
 }
