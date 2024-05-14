a66769b3baeaee20cfcf29542f391723721d7fcf
Tarun <75080636+repulsinator@users.noreply.github.com>
2024-05-14 23:29:51 +0530

    ipfs component

---
+++ src/pages/pinata.config.js
@@ -0,0 +1,8 @@
+const pinataSDK = require('pinata-sdk');
+
+const pinata = new pinataSDK(
+  '7d3fb781a5ac16107b0a',
+  'f0c25e91381ee6ca7427495577d72aaa90bacd18b8bb493f2ae58ec812ce28aa'
+);
+
+module.exports = pinata;
